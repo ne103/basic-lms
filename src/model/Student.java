3cd82b326178c3c23ee6a1713a8b7fc00814a62a
sillysillyman <sillysillyman.cs@gmail.com>
2024-05-07 15:01:06 +0900

    update Student to have their own report

--- src/model/Student.java
+++ src/model/Student.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 public class Student {
     private final int id;
     private String name;
-    private ArrayList<Subject> subjectList;
+    private Report report;
 
     public Student(int id, String name) {
         this.id = id;
         this.name = name;
-        this.subjectList = new ArrayList<>();
+        this.report = new Report();
     }
 
     public int getId() {
@@ -21,12 +21,8 @@ public class Student {
         return name;
     }
 
-    public ArrayList<Subject> getSubjectList() {
-        return subjectList;
+    public Report getReport() {
+        return report;
     }
 
-    // 매개변수로 들어온 과목이 학생이 수강중인 과목인지 아닌지 판별합니다.
-    public boolean checkSubjectExist(Subject subject) {
-        return subjectList.contains(subject);
-    }
 }
