fb67efe9a565242a01a90de634d37f269c7a0a11
sillysillyman <sillysillyman.cs@gmail.com>
2024-05-07 19:50:30 +0900

    add prinScore function and format code

--- src/model/Score.java
+++ src/model/Score.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.Map.Entry;
 
 public class Score {
-//    private final int id;
+
+    //    private final int id;
     private final int subjectId;
     private final int studentId;
-//    private int round;
+    //    private int round;
     private ArrayList<Entry<Integer, String>> scores;
 
     public Score(int id, Subject subject, int studentId, ArrayList<Entry<Integer, String>> scores) {
@@ -33,19 +34,27 @@ public class Score {
         return scores;
     }
 
+    // Score print 하는 함수
+    public void printScore() {
+        for (int i = 0; i < scores.size(); i++) {
+            System.out.printf("%3d: ", i + 1);
+            System.out.printf("%10s", scores.get(i).getKey());
+            System.out.printf("%10s\n", scores.get(i).getValue());
+        }
+    }
 
     /*
-    * 등급 산정하는 함수
-    * score가 변동이 있을때 실행되어야 합니다.*/
-    private String scoreToGrade(int subjectId, int score){
+     * 등급 산정하는 함수
+     * score가 변동이 있을때 실행되어야 합니다.*/
+    private String scoreToGrade(int subjectId, int score) {
         Subject subject = Subject.findById(subjectId);
         if (subject == null) {
             throw new IllegalArgumentException();// 예외처리?
         }
         if (subject.isEssential()) {
-            if (score >= 95){
+            if (score >= 95) {
                 return "A";
-            } else if(score >= 90){
+            } else if (score >= 90) {
                 return "B";
             } else if (score >= 80) {
                 return "C";
@@ -57,9 +66,9 @@ public class Score {
                 return "N";
             }
         } else {
-            if (score >= 90){
+            if (score >= 90) {
                 return "A";
-            } else if(score >= 80){
+            } else if (score >= 80) {
                 return "B";
             } else if (score >= 70) {
                 return "C";
