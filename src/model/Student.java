1e54a13945858b3d4202cc2408f899ef0e2db061
sillysillyman <sillysillyman.cs@gmail.com>
2024-05-07 15:41:52 +0900

    add subjectList again since it is needed when creating

--- src/model/Student.java
+++ src/model/Student.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
 public class Student {
     private final int id;
     private String name;
+    private ArrayList<Subject> subjectList;
     private Report report;
 
     public Student(int id, String name) {
         this.id = id;
         this.name = name;
+        this.subjectList = new ArrayList<>();
         this.report = new Report();
     }
 
@@ -21,8 +23,16 @@ public class Student {
         return name;
     }
 
+    public ArrayList<Subject> getSubjectList() {
+        return subjectList;
+    }
+
     public Report getReport() {
         return report;
     }
 
+    // 매개변수로 들어온 과목이 학생이 수강중인 과목인지 아닌지 판별합니다.
+    public boolean hasSubject(Subject subject) {
+        return subjectList.contains(subject);
+    }
 }
