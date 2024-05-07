4d089a5753177f3a6940ed7e93e27881974980a0
sillysillyman <sillysillyman.cs@gmail.com>
2024-05-07 19:49:43 +0900

    implement inquireRoundGradeBySubject and format code

--- src/CampManagementApplication.java
+++ src/CampManagementApplication.java
@@ -1,4 +1,5 @@
-
+import java.util.Map;
+import model.Report;
 import model.Score;
 import model.Student;
 import model.Subject;
@@ -10,12 +11,9 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * Notification
- * Java, 객체지향이 아직 익숙하지 않은 분들은 위한 소스코드 틀입니다.
- * main 메서드를 실행하면 프로그램이 실행됩니다.
- * model 의 클래스들과 아래 (// 기능 구현...) 주석 부분을 완성해주세요!
- * 프로젝트 구조를 변경하거나 기능을 추가해도 괜찮습니다!
- * 구현에 도움을 주기위한 Base 프로젝트입니다. 자유롭게 이용해주세요!
+ * Notification Java, 객체지향이 아직 익숙하지 않은 분들은 위한 소스코드 틀입니다. main 메서드를 실행하면 프로그램이 실행됩니다. model 의 클래스들과
+ * 아래 (// 기능 구현...) 주석 부분을 완성해주세요! 프로젝트 구조를 변경하거나 기능을 추가해도 괜찮습니다! 구현에 도움을 주기위한 Base 프로젝트입니다. 자유롭게
+ * 이용해주세요!
  */
 
 /*
@@ -25,6 +23,7 @@ import java.util.Scanner;
  * 전체적인 틀에 대해 궁금하신 점이나 이상한게 있다면 꼭 말씀해주세요!
  * */
 public class CampManagementApplication {
+
     // 데이터 저장소
     private static StudentRepository studentRepository = new StudentRepository();
     private static ScoreRepository scoreRepository = new ScoreRepository();
@@ -106,7 +105,7 @@ public class CampManagementApplication {
         studentRepository.getStudentID(idList);
         //새로 등록할 ID가 기존 ID 리스트에 포함되있는지 확인
         boolean success = false;
-        while(!success) {
+        while (!success) {
             if (idList.contains(studentIndex)) {
                 studentIndex++; //있으면 ID값 1증가 후 루프로 돌아가 다시 검증
             } else {
@@ -114,10 +113,10 @@ public class CampManagementApplication {
             }
         }
         //학생 객체 생성 후 ID값 1증가
-        Student student = new Student(studentIndex++,studentName);
+        Student student = new Student(studentIndex++, studentName);
         success = false;
         //조건을 충족하는 과목들을 선택할때 까지 반복
-        while(!success) {
+        while (!success) {
             //과목 선택
             System.out.println("3개 이상의 필수 과목, 2개 이상의 선택 과목 선택");
             System.out.println("필수 과목 : 1.JAVA, 2.OOP, 3.SPRING, 4.JPA, 5.MYSQL");
@@ -127,19 +126,46 @@ public class CampManagementApplication {
             //리스트 생성 후 선택한 과목을 추가
             ArrayList<Subject> subjectList = new ArrayList<>();
             // 추가로 필수과목,선택과목 개수 세기
-            int essential=0;
-            int select=0;
+            int essential = 0;
+            int select = 0;
             for (String s : subject) {
                 switch (s) {
-                    case "1" -> {subjectList.add(Subject.JAVA); essential++;}
-                    case "2" -> {subjectList.add(Subject.OOP); essential++;}
-                    case "3" -> {subjectList.add(Subject.SPRING); essential++;}
-                    case "4" -> {subjectList.add(Subject.JPA); essential++;}
-                    case "5" -> {subjectList.add(Subject.MYSQL); essential++;}
-                    case "6" -> {subjectList.add(Subject.DEGINE_PATTERN); select++;}
-                    case "7" -> {subjectList.add(Subject.SPRING_SECURITY); select++;}
-                    case "8" -> {subjectList.add(Subject.REDIS); select++;}
-                    case "9" -> {subjectList.add(Subject.MONGODB); select++;}
+                    case "1" -> {
+                        subjectList.add(Subject.JAVA);
+                        essential++;
+                    }
+                    case "2" -> {
+                        subjectList.add(Subject.OOP);
+                        essential++;
+                    }
+                    case "3" -> {
+                        subjectList.add(Subject.SPRING);
+                        essential++;
+                    }
+                    case "4" -> {
+                        subjectList.add(Subject.JPA);
+                        essential++;
+                    }
+                    case "5" -> {
+                        subjectList.add(Subject.MYSQL);
+                        essential++;
+                    }
+                    case "6" -> {
+                        subjectList.add(Subject.DEGINE_PATTERN);
+                        select++;
+                    }
+                    case "7" -> {
+                        subjectList.add(Subject.SPRING_SECURITY);
+                        select++;
+                    }
+                    case "8" -> {
+                        subjectList.add(Subject.REDIS);
+                        select++;
+                    }
+                    case "9" -> {
+                        subjectList.add(Subject.MONGODB);
+                        select++;
+                    }
                 }
             }
             //과목 개수로 조건 충족 확인
@@ -179,14 +205,7 @@ public class CampManagementApplication {
             switch (input) {
                 case 1 -> createScore(); // 수강생의 과목별 시험 회차 및 점수 등록
                 case 2 -> updateRoundScoreBySubject(); // 수강생의 과목별 회차 점수 수정
-                case 3 -> {
-                    System.out.println("조회할 과목의 ID를 입력해 주세요. (ID: 과목 이름)");
-                    for (Subject subject : Subject.values()) {
-                        System.out.println(subject.getId() + ": " + subject.name());
-                    }
-                    System.out.print("과목 ID 입력: ");
-                    inquireRoundGradeBySubject(Subject.findById(sc.nextInt())); // 수강생의 특정 과목 회차별 등급 조회
-                }
+                case 3 -> inquireRoundGradeBySubject();
                 case 4 -> flag = false; // 메인 화면 이동
                 default -> {
                     System.out.println("잘못된 입력입니다.\n메인 화면 이동...");
@@ -203,21 +222,21 @@ public class CampManagementApplication {
         int studentId = sc.nextInt();
         sc.nextLine();
         Student student = studentRepository.findById(studentId);
-        if (student == null){
+        if (student == null) {
             System.out.println(studentId + "번 학생은 존재하지 않습니다.");
             return;
         }
 
         // 과목 id 입력 및 존재 확인, 해당과목 수강하는지 확인
-        System.out.print(student.getName()+" 학생의 추가할 점수의 과목 id를 입력하세요: ");
+        System.out.print(student.getName() + " 학생의 추가할 점수의 과목 id를 입력하세요: ");
         int subjectId = sc.nextInt();
         sc.nextLine();
         Subject subject = Subject.findById(subjectId);
-        if(subject == null){
+        if (subject == null) {
             System.out.println("존재하지 않는 과목 id 입니다.");
             return;
         }
-        if(!student.checkSubjectExist(subject)){
+        if (!student.checkSubjectExist(subject)) {
             System.out.println("이 학생은 해당 과목을 수강하지 않았습니다.");
             return;
         }
@@ -226,12 +245,12 @@ public class CampManagementApplication {
         System.out.print(student.getName() + " 학생의 " + subject.name() + " 과목의 회차를 입력하세요:");
         int round = sc.nextInt();
         sc.nextLine();
-        if (round < 1 || round > 10 ){
+        if (round < 1 || round > 10) {
             System.out.println("회차는 1~10까지만 존재합니다.");
             return;
         }
         ArrayList<Score> scoreList = scoreRepository.findByIds(subjectId, studentId);
-        if(scoreRepository.checkRoundIsExist(scoreList, round)){
+        if (scoreRepository.checkRoundIsExist(scoreList, round)) {
             System.out.println("이미 존재하는 회차입니다. 등록할 수 없습니다.");
             return;
         }
@@ -239,12 +258,12 @@ public class CampManagementApplication {
         System.out.print("등록할 점수를 입력해주세요: ");
         int score = sc.nextInt();
         sc.nextLine();
-        if (score < 0 || score > 100){
+        if (score < 0 || score > 100) {
             System.out.println("잘못된 점수입니다.");
             return;
         }
 
-        scoreRepository.create(studentId,subject,round,score);
+        scoreRepository.create(studentId, subject, round, score);
 
     }
 
@@ -324,13 +343,42 @@ public class CampManagementApplication {
     }
 
     // 수강생의 특정 과목 회 차별 등급 조회
-    private static void inquireRoundGradeBySubject(Subject subject) {
+    private static void inquireRoundGradeBySubject() {
         // 기능 구현 (조회할 특정 과목)
         System.out.println("회차별 등급을 조회합니다...");
         // 기능 구현
+        System.out.println("조회할 수강생의 이름을 입력해주세요.");
+        System.out.print("이름 입력: ");
+        String name = sc.next();
+
+        int studentId;
+        ArrayList<Student> students = studentRepository.findByName(sc.next());
+        if (students.isEmpty()) {
+            return; // 이름 발견 안 됐을 때 입력 다시 받도록 구현 필요.
+        } else if (students.size() == 1) {
+            studentId = students.get(0).getId();
+        } else {
+            System.out.println("중복된 이름이 발견되었습니다. ID를 입력해주세요.");
+            for (Student student : students) {
+                System.out.println(student.getId() + ". " + student.getName());
+            }
+            studentId = sc.nextInt(); // students에 없는 id 입력 가능하기 때문에 studentId 유효성 검증 추가 구현 필요.
+            sc.nextLine();
+        }
+
+        Student student = studentRepository.findById(studentId);
+        Map<Subject, Score> report = student.getReport().getReport();
+
+        System.out.println("조회할 과목의 ID를 입력해 주세요. (ID: 과목 이름)");
+        for (Subject subject : student.getSubjectList()) {
+            System.out.println(subject.getId() + ": " + subject.name());
+        }
+        System.out.print("과목 ID 입력: ");
+        int subjectId = sc.nextInt();
+        Score score = report.get(Subject.findById(subjectId));
+        score.printScore();
         System.out.println("\n등급 조회 성공!");
     }
-
 
 
 }
